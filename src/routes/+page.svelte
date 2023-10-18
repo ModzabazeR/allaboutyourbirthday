eca0c2855eaba4b9e71d37ae82cbcb0f0a16b432
ModzabazeR <moddaeng.pps@gmail.com>
2023-10-18 16:18:23 +0700

    Initial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section class="flex h-screen justify-center items-center text-center">
+	<h1 class="text-4xl">Hello</h1>
+</section>
