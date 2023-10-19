12e762f89ce64cd79d6f06a480c1e1e9e764fc9a
M <xboc12349asd@gmail.com>
2023-10-19 22:17:18 +0700

    upday

--- src/lib/data/floridaman.ts
+++ src/lib/data/floridaman.ts
@@ -7,8 +7,8 @@ export const floridaman: IFloridaman[] = [
 		data: [
 			{
 				title: "Florida man who attacked McDonald's worker over straw sentenced to jail",
-				link: "https://www.foxnews.com/food-drink/florida-man-who-attacked-mcdonalds-worker-over-straw-sentenced-to-jail",
-                year: 2019,
+				link: 'https://www.foxnews.com/food-drink/florida-man-who-attacked-mcdonalds-worker-over-straw-sentenced-to-jail',
+				year: 2019
 			}
 		]
 	},
@@ -17,36 +17,49 @@ export const floridaman: IFloridaman[] = [
 		month: 1,
 		data: [
 			{
-				title: "Florida man robs store dressed as Spiderman",
-				link: "https://abc7chicago.com/florida-man-spiderman-robs-store-casselberry-seminole-county-sheriffs-office/5194105/",
+				title: 'Florida man robs store dressed as Spiderman',
+				link: 'https://abc7chicago.com/florida-man-spiderman-robs-store-casselberry-seminole-county-sheriffs-office/5194105/'
 			},
-            {
-                title: "Florida Man Accused of Attacking Girlfriend With Banana",
-                link: "https://www.nbcmiami.com/news/local/florida-man-accused-of-attacking-girlfriend-with-banana/118271/",
-                year: 2014,
-            }
+			{
+				title: 'Florida Man Accused of Attacking Girlfriend With Banana',
+				link: 'https://www.nbcmiami.com/news/local/florida-man-accused-of-attacking-girlfriend-with-banana/118271/',
+				year: 2014
+			}
 		]
 	},
-    {
-        day: 3,
-        month: 1,
-        data: [
-            {
-                title: "Florida Man Arrested at Mar-a-lago, Says He Came to Talk to Trump About 'His $6.3 Trillion'",
-                link: "https://www.local10.com/news/2019/01/03/florida-man-arrested-at-mar-a-lago-says-he-came-to-talk-to-trump-about-his-63-trillion/",
-                year: 2019,
-            }
-        ]
-    },
-    {
-        day: 4,
-        month: 1,
-        data: [
-            {
-                title: "Florida man arrested after hitting dad with pizza because he was mad he helped birth him",
-                link: "https://www.abcactionnews.com/news/region-pasco/florida-man-arrested-after-hitting-dad-with-pizza-because-he-was-mad-he-helped-birth-him",
-                year: 2019,
-            }
-        ]
-    }
+	{
+		day: 3,
+		month: 1,
+		data: [
+			{
+				title:
+					"Florida Man Arrested at Mar-a-lago, Says He Came to Talk to Trump About 'His $6.3 Trillion'",
+				link: 'https://www.local10.com/news/2019/01/03/florida-man-arrested-at-mar-a-lago-says-he-came-to-talk-to-trump-about-his-63-trillion/',
+				year: 2019
+			}
+		]
+	},
+	{
+		day: 4,
+		month: 1,
+		data: [
+			{
+				title:
+					'Florida man arrested after hitting dad with pizza because he was mad he helped birth him',
+				link: 'https://www.abcactionnews.com/news/region-pasco/florida-man-arrested-after-hitting-dad-with-pizza-because-he-was-mad-he-helped-birth-him',
+				year: 2019
+			}
+		]
+	},
+	{
+		day: 5,
+		month: 1,
+		data: [
+			{
+				title: '74-year-old Florida man on electric scooter threatened, chased 2 with knife',
+				link: 'https://www.clickorlando.com/news/local/2022/01/05/74-year-old-florida-man-on-electric-scooter-threatened-chased-2-with-knife-police-say/',
+				year: 2022
+			}
+		]
+	}
 ];
