66d31e626379319b7a418eddbade45a4cf64d23f
daisuki0i <daisuki0i@daisuki0is-MacBook-Pro.local>
2023-10-20 01:20:00 +0700

    Until 10 July

--- src/lib/data/floridaman.ts
+++ src/lib/data/floridaman.ts
@@ -61,5 +61,120 @@ export const floridaman: IFloridaman[] = [
 				year: 2022
 			}
 		]
+	},
+	{
+		day: 1,
+		month: 7,
+		data: [
+			{
+				title:
+					'Florida man arrested after pelting girlfriend with McDonald s sweet and sour packets',
+				link: 'https://www.foxnews.com/us/florida-man-arrested-for-pelting-girlfriend-with-mcdonalds-sweet-and-sour-packets-report',
+				year: 2019
+			}
+		]
+	},
+	{
+		day: 2,
+		month: 7,
+		data: [
+			{
+				title: 'Florida Man Sits On Gun, Shoots Himself In Penis',
+				link: 'https://www.cbsnews.com/miami/news/man-sits-gun-shoots-himself-penis/',
+				year: 2017
+			}
+		]
+	},
+	{
+		day: 3,
+		month: 7,
+		data: [
+			{
+				title:
+					'Florida man with ‘hustle eat sleep repeat’ tattoo charged with throwing hot dog at police officer',
+				link: 'https://www.foxnews.com/us/florida-man-charged-throwing-hot-dog-officer',
+				year: 2022
+			}
+		]
+	},
+	{
+		day: 4,
+		month: 7,
+		data: [
+			{
+				title:
+					'Florida man accused of pulling gun, pointing laser sight at neighbors for shooting fireworks',
+				link: 'https://www.fox35orlando.com/news/florida-man-arrested-for-threatening-neighbors-with-a-gun-about-fireworks-deputies-say',
+				year: 2022
+			}
+		]
+	},
+	{
+		day: 5,
+		month: 7,
+		data: [
+			{
+				title: "Florida man pretending to be a cop pulls over real deputy, sheriff's",
+				link: 'https://www.wctv.tv/content/news/Florida-man-pretending-to-be-a-cop-pulls-over-real-deputy-sheriffs-office-says-512255742.html',
+				year: 2019
+			}
+		]
+	},
+	{
+		day: 6,
+		month: 7,
+		data: [
+			{
+				title:
+					'Florida man steals reporter’ s car, crashes it into North Carolina police cruiser amid manhunt',
+				link: 'https://www.foxnews.com/us/man-steals-reporters-car-and-crashes-it-into-police-cruiser-amid-manhunt',
+				year: 2021
+			}
+		]
+	},
+	{
+		day: 7,
+		month: 7,
+		data: [
+			{
+				title:
+					'Florida Man Haunts Beaches Dressed As Grim Reaper Says Governor Should Require Masks Statewide',
+				link: 'Florida man linked to Proud Boys gets 5 years for pepper-spraying police during Capitol riot',
+				year: 2023
+			}
+		]
+	},
+	{
+		day: 8,
+		month: 7,
+		data: [
+			{
+				title: 'Florida man loses pants while being chased by cops in hours-long manhunt',
+				link: 'https://nypost.com/2022/07/08/florida-man-jesse-lamb-flees-nude-from-police-in-hours-long-manhunt/',
+				year: 2022
+			}
+		]
+	},
+	{
+		day: 9,
+		month: 7,
+		data: [
+			{
+				title: 'Florida Man Sentenced to 20 Years for Uploading Bomb-Making Video',
+				link: 'https://www.nytimes.com/2022/07/09/us/florida-man-isis.html',
+				year: 2022
+			}
+		]
+	},
+	{
+		day: 10,
+		month: 7,
+		data: [
+			{
+				title: 'Florida man tries to flee deputies on a riding lawn mower',
+				link: 'https://www.clickorlando.com/news/florida/2022/07/11/florida-man-tries-to-flee-deputies-on-a-riding-lawn-mower/',
+				year: 2022
+			}
+		]
 	}
 ];
