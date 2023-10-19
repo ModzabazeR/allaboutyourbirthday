e8b18af320f9ebca0aede3af0676b3813f7421dc
ModzabazeR <moddaeng.pps@gmail.com>
2023-10-19 14:31:04 +0700

    add florida man sample data

---
+++ src/lib/data/floridaman.ts
@@ -0,0 +1,14 @@
+import type { IFloridaman } from '$lib/types/dataInterfaces';
+
+export const floridaman: IFloridaman[] = [
+	{
+		day: 1,
+		month: 1,
+		data: [
+			{
+				title: "Florida man who attacked McDonald's worker over straw sentenced to jail",
+				link: 'https://www.foxnews.com/food-drink/florida-man-who-attacked-mcdonalds-worker-over-straw-sentenced-to-jail'
+			},
+		]
+	}
+];
---
+++ src/lib/types/dataInterfaces.ts
@@ -0,0 +1,10 @@
+export interface IFloridaman {
+    day: number;
+    month: number;
+    data: IFloridamanData[];
+}
+
+export interface IFloridamanData {
+    title: string;
+    link: string;
+}
