1cfbf13a1da0ac6c518cfb57e0328f20739c7bc2
ModzabazeR <moddaeng.pps@gmail.com>
2023-10-19 13:58:59 +0700

    test passed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,5 +4,5 @@
 </svelte:head>
 
 <section class="flex flex-col h-screen justify-center items-center text-center">
-	<h1 class="text-4xl text-green-500">Hello from Docker! hehe</h1>
+	<h1 class="text-4xl text-green-500">Hello from Docker!</h1>
 </section>
