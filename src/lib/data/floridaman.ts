6360d7a6b943e9384c3445961724058bc49f9725
ModzabazeR <moddaeng.pps@gmail.com>
2023-10-21 00:14:23 +0700

    january complete

--- src/lib/data/floridaman.ts
+++ src/lib/data/floridaman.ts
@@ -61,5 +61,324 @@ export const floridaman: IFloridaman[] = [
 				year: 2022
 			}
 		]
+	},
+	{
+		day: 6,
+		month: 1,
+		data: [
+			{
+				title:
+					'Florida man who beat officers with flagpole during Jan. 6 riot gets four years in prison',
+				link: 'https://www.nbcdfw.com/news/national-international/florida-man-who-beat-officers-with-flagpole-during-jan-6-riot-gets-four-years-in-prison/3319120/',
+				year: 2021
+			}
+		]
+	},
+	{
+		day: 7,
+		month: 1,
+		data: [
+			{
+				title: 'Florida man denies syringes found in rectum are his',
+				link: 'https://www.local10.com/news/2019/01/07/florida-man-denies-syringes-found-in-rectum-are-his/',
+				year: 2019
+			}
+		]
+	},
+	{
+		day: 8,
+		month: 1,
+		data: [
+			{
+				title: 'Florida man arrested after argument over cheesesteak',
+				link: 'https://www.local10.com/news/2019/01/08/florida-man-arrested-after-argument-over-cheesesteak/',
+				year: 2019
+			}
+		]
+	},
+	{
+		day: 9,
+		month: 1,
+		data: [
+			{
+				title: 'Florida Man Accused of Burning Son to Teach Him Lesson About Fire',
+				link: 'https://www.wtsp.com/article/news/regional/florida/florida-man-accused-of-burning-son-to-teach-him-lesson-about-fire/67-15a4de8c-d2fb-45be-a35e-a0a073ba33c0',
+				year: 2019
+			}
+		]
+	},
+	{
+		day: 10,
+		month: 1,
+		data: [
+			{
+				title: 'Florida Man Allegedly Fooled Family Into Believing Murdered Wife Was Still Alive',
+				link: 'https://www.rollingstone.com/culture/culture-news/shelby-svensen-florida-wife-family-murder-777245/',
+				year: 2019
+			}
+		]
+	},
+	{
+		day: 11,
+		month: 1,
+		data: [
+			{
+				title:
+					'Florida man charged with battery in fight over peanut butter sandwich, spilled milk',
+				link: 'https://www.clickorlando.com/news/2016/01/11/cops-florida-man-charged-with-battery-in-fight-over-peanut-butter-sandwich-spilled-milk/',
+				year: 2016
+			},
+			{
+				title: 'Florida man chews up police car seat after cocaine arrest',
+				link: 'https://wgno.com/us-world-news/florida-man-chews-up-police-car-seat-after-cocaine-arrest/',
+				year: 2019
+			}
+		]
+	},
+	{
+		day: 12,
+		month: 1,
+		data: [
+			{
+				title:
+					'Florida man accidentally shoots himself in the leg while allegedly attempting to burglarize a car',
+				link: 'https://www.cbsnews.com/news/florida-man-accidentally-shoots-himself-in-the-leg-while-allegedly-attempting-burglary-car-okaloosa-county/',
+				year: 2023
+			},
+			{
+				title: 'Florida driver finds boa constrictor in his car engine',
+				link: 'https://www.wfla.com/news/florida/florida-driver-finds-boa-constrictor-in-his-car-engine/1699749944/',
+				year: 2019
+			}
+		]
+	},
+	{
+		day: 13,
+		month: 1,
+		data: [
+			{
+				title: 'Florida Man Arrested for Stealing Floating Tiki Hut',
+				link: 'https://vinepair.com/booze-news/florida-man-steals-floating-tiki-hut/',
+				year: 2021
+			}
+		]
+	},
+	{
+		day: 14,
+		month: 1,
+		data: [
+			{
+				title: "Florida man threatens to kill man 'with kindness,' uses machete named 'Kindness'",
+				link: 'https://www.local10.com/news/2019/01/14/florida-man-threatens-to-kill-man-with-kindness-uses-machete-named-kindness/',
+				year: 2019
+			}
+		]
+	},
+	{
+		day: 15,
+		month: 1,
+		data: [
+			{
+				title: "Florida Man Causes Highway Crash, Steals Good Samaritan's Truck Who Stopped To Help",
+				link: 'https://www.cbsnews.com/miami/news/florida-man-causes-crash-steals-truck/',
+				year: 2019
+			}
+		]
+	},
+	{
+		day: 16,
+		month: 1,
+		data: [
+			{
+				title: "Florida man who drove Ferrari into water said ‘Jesus told him to’",
+				link: 'https://nbc-2.com/nbc-2-wbbh/2019/01/16/florida-man-who-drove-ferrari-into-water-said-jesus-told-him-to/',
+				year: 2019
+			}
+		]
+	},
+	{
+		day: 17,
+		month: 1,
+		data: [
+			{
+				title: "Florida man chased down by horses while eluding arrest",
+				link: 'https://www.clickorlando.com/news/2019/01/17/video-florida-man-chased-down-by-horses-while-eluding-arrest-deputies-say/',
+				year: 2019
+			}
+		]
+	},
+	{
+		day: 18,
+		month: 1,
+		data: [
+			{
+				title: "Florida man arrested for DUI after mistaking bank drive-thru for Taco Bell",
+				link: 'https://www.10news.com/news/national/florida-man-arrested-for-dui-after-mistaking-bank-drive-thru-for-taco-bell',
+				year: 2018
+			}
+		]
+	},
+	{
+		day: 19,
+		month: 1,
+		data: [
+			{
+				title: "Florida Man Accused Of Throwing Chair Across Steak 'n Shake",
+				link: 'https://patch.com/florida/newportrichey/port-richey-man-accused-throwing-chair-across-steak-n-shake',
+				year: 2019
+			}
+		]
+	},
+	{
+		day: 20,
+		month: 1,
+		data: [
+			{
+				title: "Florida man attempts to hide underwater from cops, gets arrested after coming up for air",
+				link: 'https://www.foxnews.com/us/florida-man-underwater-escape-police-arrested',
+				year: 2020
+			}
+		]
+	},
+	{
+		day: 21,
+		month: 1,
+		data: [
+			{
+				title: "Florida man known as ‘The Monkey Whisperer’ arrested for illegally selling primates",
+				link: 'https://www.kiro7.com/news/trending/florida-man-known-monkey-whisperer-arrested-illegally-selling-primates/2JXFDADYOFETJIHRYKRAJGIZVY/',
+				year: 2021
+			}
+		]
+	},
+	{
+		day: 22,
+		month: 1,
+		data: [
+			{
+				title: "Florida man accused of stealing crossbow by shoving it down his sweatpants",
+				link: 'https://www.the-independent.com/news/world/americas/crime/florida-man-crossbow-pants-trousers-b2022808.html',
+				year: 2022
+			}
+		]
+	},
+	{
+		day: 23,
+		month: 1,
+		data: [
+			{
+				title: "North Florida Man Beat, Pepper Sprayed Mom Because 'She Was a Narcissist'",
+				link: 'https://www.nbcmiami.com/news/local/north-florida-man-beats-pepper-sprays-mom-because-she-was-a-narcissist-police/5194/',
+				year: 2019
+			}
+		]
+	},
+	{
+		day: 24,
+		month: 1,
+		data: [
+			{
+				title: "Florida man in Easter Bunny outfit arrested after hit-and-run crash",
+				link: 'https://www.kiro7.com/news/trending/florida-man-easter-bunny-outfit-arrested-after-hit-and-run-crash-troopers-say/ZIX54JLGQNBHVDGF3I2GXK2DQ4/',
+				year: 2020
+			}
+		]
+	},
+	{
+		day: 25,
+		month: 1,
+		data: [
+			{
+				title: "Florida man arrested at Waffle House after alleged outburst over bacon",
+				link: 'https://nypost.com/2022/01/25/florida-man-arrested-at-waffle-house-after-over-bacon/',
+				year: 2022
+			}
+		]
+	},
+	{
+		day: 26,
+		month: 1,
+		data: [
+			{
+				title: "Florida Man Finds a WWII Grenade, Places It in His Truck, Drives to Taco Bell",
+				link: 'https://www.thrillist.com/news/nation/florida-man-finds-wwii-grenade-drives-to-taco-bell',
+				year: 2019
+			}
+		]
+	},
+	{
+		day: 27,
+		month: 1,
+		data: [
+			{
+				title: "Florida Man Prompts Evacuation at Taco Bell After Bringing Grenade He Found While Fishing",
+				link: 'https://ktla.com/news/florida-man-prompts-evacuation-at-taco-bell-after-bringing-grenade-he-found-while-fishing/',
+				year: 2019
+			}
+		]
+	},
+	{
+		day: 28,
+		month: 1,
+		data: [
+			{
+				title: "Florida man threatened employees with gun when chicken wings order was late",
+				link: 'https://www.kiro7.com/news/trending/florida-man-threatened-employees-with-gun-when-chicken-wings-order-was-late-police-say/SOG2Q4LLPZBJ5NLEQ3URT2XRJM/',
+				year: 2023
+			},
+			{
+				title: "Florida man thought he stole opioids, got laxatives instead",
+				link: 'https://www.wtxl.com/news/police-florida-man-thought-he-stole-opioids-got-laxatives-instead/article_4154ccb6-234c-11e9-b587-23388e4a8b84.html',
+				year: 2019
+			}
+		]
+	},
+	{
+		day: 29,
+		month: 1,
+		data: [
+			{
+				title: "Florida man sentenced to jail and moral therapy for attacking a Minion",
+				link: 'https://www.orlandoweekly.com/news/florida-man-sentenced-to-jail-and-moral-therapy-for-attacking-a-minion-23602592',
+				year: 2019
+			},
+			{
+				title: "Florida man accused of robbing Chinese restaurant at finger point",
+				link: 'https://www.wtsp.com/article/news/weird/wtflorida/florida-man-accused-of-robbing-chinese-restaurant-at-finger-point/67-11cb0cbd-b7b9-4581-8d05-547a873b3744',
+				year: 2019
+			}
+		]
+	},
+	{
+		day: 30,
+		month: 1,
+		data: [
+			{
+				title: "Florida man buys winning $1 million scratch-off game after being cut in line",
+				link: 'https://www.ksat.com/news/national/2023/01/30/florida-man-buys-winning-1-million-scratch-off-game-after-being-cut-in-line/',
+				year: 2023
+			},
+			{
+				title: "Florida man spent weeks in jail for heroin that was actually detergent",
+				link: 'https://www.wfla.com/news/florida/florida-man-spent-weeks-in-jail-for-heroin-that-was-actually-detergent/1739544148/',
+				year: 2019
+			}
+		]
+	},
+	{
+		day: 31,
+		month: 1,
+		data: [
+			{
+				title: "Florida Man With Florida Tattoo On Forehead Arrested For Misuse Of 911, Marijuana Possession",
+				link: 'https://www.cbsnews.com/miami/news/florida-man-florida-tattoo-forehead-misusing-911-marijuana-possession/',
+				year: 2021
+			},
+			{
+				title: "Florida man thought he’d do donuts on an airport runway. Police weren’t amused",
+				link: 'https://www.miamiherald.com/news/state/florida/article225336285.html',
+				year: 2019
+			}
+		]
 	}
 ];
