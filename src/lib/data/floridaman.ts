65b1b7886b435f08408ab0c49e0f82fbd9889ce5
daisuki0i <daisuki0i@daisuki0is-MacBook-Pro.local>
2023-10-21 00:08:35 +0700

    July completed

--- src/lib/data/floridaman.ts
+++ src/lib/data/floridaman.ts
@@ -176,5 +176,241 @@ export const floridaman: IFloridaman[] = [
 				year: 2022
 			}
 		]
+	},
+	{
+		day: 11,
+		month: 7,
+		data: [
+			{
+				title: 'Florida man with no arms charged with stabbing man with scissors',
+				link: 'https://www.aol.com/article/news/2018/07/11/florida-man-with-no-arms-charged-with-stabbing-man-with-scissors/23480050/',
+				year: 2018
+			}
+		]
+	},
+	{
+		day: 12,
+		month: 7,
+		data: [
+			{
+				title: 'Florida Man Sentenced in Bomb Plot',
+				link: 'https://www.adl.org/resources/news/florida-man-sentenced-bomb-plot',
+				year: 2004
+			}
+		]
+	},
+	{
+		day: 13,
+		month: 7,
+		data: [
+			{
+				title: 'Florida man arrested after writing himself almost half a million in company checks',
+				link: 'https://wach.com/news/local/man-arrestedcharged-after-writing-himself-almost-half-a-million-in-company-checks',
+				year: 2021
+			}
+		]
+	},
+	{
+		day: 14,
+		month: 7,
+		data: [
+			{
+				title: 'Florida man caught hiding heroin, crack in his buttocks',
+				link: 'https://www.fox61.com/article/news/local/outreach/awareness-months/florida-man-caught-hiding-heroin-crack-in-his-buttocks-deputies-say/520-8777af39-1f49-4905-a0c4-3102652c962d',
+				year: 2019
+			}
+		]
+	},
+	{
+		day: 15,
+		month: 7,
+		data: [
+			{
+				title:
+					'Florida man accused of stealing gator from golf course, threw it around to teach ‘it a lesson’',
+				link: 'https://www.clickorlando.com/news/local/2021/07/15/florida-man-accused-of-stealing-gator-from-golf-course-threw-it-around-to-teach-it-a-lesson/#//',
+				year: 2021
+			}
+		]
+	},
+	{
+		day: 16,
+		month: 7,
+		data: [
+			{
+				title: 'Florida man, 79, survives alligator bite: ‘There’s a lot of skin ripped off’',
+				link: 'https://www.theguardian.com/us-news/2023/jul/16/florida-man-survives-alligator-bite',
+				year: 2023
+			}
+		]
+	},
+	{
+		day: 17,
+		month: 7,
+		data: [
+			{
+				title: "Florida Man Arrested for Outburst Over McDonald's French Fries",
+				link: 'https://www.nbcmiami.com/news/local/florida-man-arrested-for-outburst-over-french-fries-in-mcdonalds-police/1900671/',
+				year: 2012
+			}
+		]
+	},
+	{
+		day: 18,
+		month: 7,
+		data: [
+			{
+				title: 'Florida man steals candles, sprays people with bear mace at mall',
+				link: 'https://www.clickorlando.com/news/florida/2021/07/18/florida-man-steals-candles-sprays-people-with-bear-mace-at-mall-police-say/',
+				year: 2021
+			}
+		]
+	},
+	{
+		day: 19,
+		month: 7,
+		data: [
+			{
+				title:
+					"Florida man, 33, posed as housewife to lure men into home where he'd secretly film sex acts for web",
+				link: 'https://www.foxnews.com/us/florida-man-33-posed-as-housewife-to-lure-men-into-home-where-hed-secretly-film-sex-acts-for-web-cops-say',
+				year: 2018
+			}
+		]
+	},
+	{
+		day: 20,
+		month: 7,
+		data: [
+			{
+				title: "Florida Man Says He Went 'Bananas,' Shot Out Utility Workers' Tires",
+				link: 'https://www.cbsnews.com/miami/news/florida-man-bananas-shot-out-utility-workers-tires/',
+				year: 2017
+			}
+		]
+	},
+	{
+		day: 21,
+		month: 7,
+		data: [
+			{
+				title: 'Florida man fashioned tourniquet from dog leash for cyclist bitten by alligator',
+				link: 'https://www.orlandoweekly.com/news/florida-man-fashioned-tourniquet-from-dog-leash-for-cyclist-bitten-by-alligator-29673891',
+				year: 2021
+			}
+		]
+	},
+	{
+		day: 22,
+		month: 7,
+		data: [
+			{
+				title: 'Florida man accused of steering urine tests to drug lab',
+				link: 'https://www.clickorlando.com/news/florida/2020/07/22/florida-man-accused-of-steering-urine-tests-to-drug-lab/',
+				year: 2020
+			}
+		]
+	},
+	{
+		day: 23,
+		month: 7,
+		data: [
+			{
+				title:
+					'Florida man sentenced to 3 years for spitting on 70-year-old man who asked him to social distance',
+				link: 'https://www.clickorlando.com/news/local/2021/07/23/florida-man-sentenced-to-3-years-for-spitting-on-70-year-old-man-who-asked-him-to-social-distance/',
+				year: 2021
+			}
+		]
+	},
+	{
+		day: 24,
+		month: 7,
+		data: [
+			{
+				title: 'Florida Man Shows Off Gun, Accidentaly Shoots Himself',
+				link: 'https://www.cbsnews.com/miami/news/florida-man-shows-off-gun-accidently-shoots-himself/',
+				year: 2021
+			}
+		]
+	},
+	{
+		day: 25,
+		month: 7,
+		data: [
+			{
+				title: 'Brave Florida man serves up hot dog lunch for alligators straight from his mouth',
+				link: 'https://abc13.com/florida-man-gator-the-crusader/2247372/',
+				year: 2017
+			}
+		]
+	},
+	{
+		day: 26,
+		month: 7,
+		data: [
+			{
+				title: 'Florida man arrested on DUI charge after driving scooter into Walmart shelves',
+				link: 'https://www.clickorlando.com/news/local/2022/07/26/florida-man-arrested-on-dui-charge-after-driving-scooter-into-walmart-shelves-police-say/',
+				year: 2022
+			}
+		]
+	},
+	{
+		day: 27,
+		month: 7,
+		data: [
+			{
+				title: 'Florida Man Arrested On Allegations He Used COVID-Relief Funds To Buy Lamborghini',
+				link: 'https://www.cbsnews.com/miami/news/florida-man-arrested-allegations-used-covid-relief-funds-buy-lamborghini/',
+				year: 2020
+			}
+		]
+	},
+	{
+		day: 28,
+		month: 7,
+		data: [
+			{
+				title: 'Florida man makes beer run with gator in hand',
+				link: 'https://www.fox6now.com/news/florida-man-makes-beer-run-with-gator-in-hand',
+				year: 2018
+			}
+		]
+	},
+	{
+		day: 29,
+		month: 7,
+		data: [
+			{
+				title:
+					'Florida man looking for Frisbees killed in suspected alligator attack at a disc golf park',
+				link: 'https://sports.yahoo.com/florida-man-looking-frisbees-killed-121226802.html',
+				year: 2022
+			}
+		]
+	},
+	{
+		day: 30,
+		month: 7,
+		data: [
+			{
+				title: 'Man carries alligator into beer store in Florida',
+				link: 'https://www.upi.com/Odd_News/2018/07/30/Man-carries-alligator-into-beer-store-in-Florida/8771532956811/',
+				year: 2018
+			}
+		]
+	},
+	{
+		day: 31,
+		month: 7,
+		data: [
+			{
+				title:
+					'Florida man tries to buy Porsche, Rolex watches with checks printed from home computer',
+				link: 'https://www.wsbtv.com/news/trending/florida-man-tries-buy-porsche-rolex-watches-with-checks-printed-home-computer-police-say/ARZMOTU4G5HSRBFFUKK53CGYM4/',
+				year: 2020
+			}
+		]
 	}
 ];
