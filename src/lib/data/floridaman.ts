6a4fdc3be470ca21efec3055d610a1d848c47981
daisuki0i <daisuki0i@daisuki0is-MacBook-Pro.local>
2023-10-21 14:45:10 +0700

    August completed

--- src/lib/data/floridaman.ts
+++ src/lib/data/floridaman.ts
@@ -412,5 +412,360 @@ export const floridaman: IFloridaman[] = [
 				year: 2020
 			}
 		]
+	},
+	{
+		day: 1,
+		month: 8,
+		data: [
+			{
+				title:
+					'Florida man clings to the hood of a semi truck driving down an interstate for NINE MILES in insane video before troopers intercept vehicle',
+				link: 'https://www.dailymail.co.uk/news/article-8584617/Florida-man-filmed-clinging-hood-semi-truck-driving-interstate-NINE-MILES.html',
+				year: 2020
+			}
+		]
+	},
+	{
+		day: 2,
+		month: 8,
+		data: [
+			{
+				title: 'Florida man jailed after dispute over hedgehog leads to armed confrontation',
+				link: 'https://www.cbsnews.com/news/florida-man-jailed-after-dispute-over-hedgehog-leads-to-armed-confrontation/',
+				year: 2017
+			}
+		]
+	},
+	{
+		day: 3,
+		month: 8,
+		data: [
+			{
+				title: 'Florida man ‘Redbeard’ steals $1,800 parrot, abandons it',
+				link: 'https://nypost.com/2022/08/03/florida-man-named-redbeard-steals-1800-parrot-authorities/',
+				year: 2022
+			}
+		]
+	},
+	{
+		day: 4,
+		month: 8,
+		data: [
+			{
+				title: 'Florida man who ran down robber acquitted',
+				link: 'https://www.upi.com/Top_News/2006/08/04/Florida-man-who-ran-down-robber-acquitted/57641154667425/',
+				year: 2006
+			}
+		]
+	},
+	{
+		day: 5,
+		month: 8,
+		data: [
+			{
+				title: "Florida Man Arrested After Biting Off Part of Tourist's Ear at Key West Hotel",
+				link: 'https://www.nbcmiami.com/news/local/florida-man-arrested-after-biting-off-part-of-tourists-ear-at-key-west-hotel/2521605/',
+				year: 2021
+			}
+		]
+	},
+	{
+		day: 6,
+		month: 8,
+		data: [
+			{
+				title: 'Florida man bites off friend’s ear in fight over woman in wheelbarrow',
+				link: 'https://finance.yahoo.com/news/florida-man-bites-off-friend-145907220.html',
+				year: 2021
+			}
+		]
+	},
+	{
+		day: 7,
+		month: 8,
+		data: [
+			{
+				title: "Florida man said he smoked THC 'because Jesus was returning'",
+				link: 'https://www.foxnews.com/us/florida-man-thc-jesus-returning',
+				year: 2019
+			}
+		]
+	},
+	{
+		day: 8,
+		month: 8,
+		data: [
+			{
+				title: "Florida Man Asks Walmart Employee For 'Anything That Would Kill 200 People'",
+				link: 'https://www.cbsnews.com/miami/news/florida-man-walmart-employee-anything-kill-200-people/',
+				year: 2019
+			},
+			{
+				title: 'Florida man accused of flooding emergency room after attacking nurse, stripping',
+				link: 'https://www.local10.com/news/florida/2023/08/08/florida-man-accused-of-flooding-emergency-room-after-attacking-nurse-stripping/',
+				year: 2023
+			}
+		]
+	},
+	{
+		day: 9,
+		month: 8,
+		data: [
+			{
+				title: 'Florida man drives golf cart into Walmart, attempts to run over people',
+				link: 'https://abc7news.com/florida-man-news-walmart-crash-golf-cart/5454877/',
+				year: 2019
+			}
+		]
+	},
+	{
+		day: 10,
+		month: 8,
+		data: [
+			{
+				title: 'Florida Man 71-Year-Old Confessed To Graffiti Spree',
+				link: 'https://www.local10.com/news/2011/08/10/police-71-year-old-florida-man-confessed-to-graffiti-spree/',
+				year: 2011
+			}
+		]
+	},
+	{
+		day: 11,
+		month: 8,
+		data: [
+			{
+				title:
+					'Florida man stole forklift, pickup, four-wheeler (and more) in ‘impressive crime spree,’',
+				link: 'https://www.clickorlando.com/news/local/2021/08/11/florida-man-stole-forklift-pickup-four-wheeler-and-more-in-impressive-crime-spree-deputies-say/',
+				year: 2021
+			}
+		]
+	},
+	{
+		day: 12,
+		month: 8,
+		data: [
+			{
+				title: "Florida Man Says He Was Fired for Daughter's Cancer Treatment",
+				link: 'https://abcnews.go.com/Business/florida-man-sues-wells-fargo-alleging-daughters-cancer/story?id=16971876',
+				year: 2012
+			}
+		]
+	},
+	{
+		day: 13,
+		month: 8,
+		data: [
+			{
+				title: 'Florida man arrested for intentionally running over ducklings playing in a puddle',
+				link: 'https://www.10news.com/news/national/florida-man-arrested-for-intentionally-running-over-ducklings-playing-in-a-puddle',
+				year: 2019
+			}
+		]
+	},
+	{
+		day: 14,
+		month: 8,
+		data: [
+			{
+				title:
+					"Florida man claiming to be Alice in Wonderland says 'hookah-smoking caterpillar' told him to destroy liquor store with forklift",
+				link: 'https://torontosun.com/2017/08/14/florida-man-claiming-to-be-alice-in-wonderland-says-hookah-smoking-caterpillar-told-him-to-destroy-liquor-store-with-forklift-cops',
+				year: 2017
+			}
+		]
+	},
+	{
+		day: 15,
+		month: 8,
+		data: [
+			{
+				title: 'Florida man sprays women with roach spray, break out nunchucks over loud music',
+				link: 'https://www.boston25news.com/news/deep-viral/police-florida-man-sprays-women-with-roach-spray-break-out-nunchucks-over-loud-music/976378694/',
+				year: 2019
+			}
+		]
+	},
+	{
+		day: 16,
+		month: 8,
+		data: [
+			{
+				title: "Florida man suspected of murdering couple was caught biting victim's face off",
+				link: 'https://www.orlandoweekly.com/news/florida-man-suspected-of-murdering-couple-was-caught-biting-victims-face-off-2516938',
+				year: 2016
+			}
+		]
+	},
+	{
+		day: 17,
+		month: 8,
+		data: [
+			{
+				title:
+					"Florida Man Displayed Gun After Learning Delray Beach McDonald's Had Broken Ice Cream Machine",
+				link: 'https://www.nbcmiami.com/news/local/florida-man-displayed-gun-after-learning-delray-beach-mcdonalds-had-broken-ice-cream-machine/21862/',
+				year: 2017
+			}
+		]
+	},
+	{
+		day: 18,
+		month: 8,
+		data: [
+			{
+				title:
+					"Florida man wielding machete dead after breaking into home, telling sleeping resident 'I love you'",
+				link: 'https://www.foxnews.com/us/florida-man-wielding-machete-dead-breaking-home-telling-sleeping-resident-i-love-you',
+				year: 2022
+			}
+		]
+	},
+	{
+		day: 19,
+		month: 8,
+		data: [
+			{
+				title:
+					'Florida man arrested after allegedly shoving steaks worth more than $50 down his pants',
+				link: 'https://www.foxnews.com/us/florida-man-shoving-steaks-down-his-pants',
+				year: 2019
+			}
+		]
+	},
+	{
+		day: 20,
+		month: 8,
+		data: [
+			{
+				title:
+					'Florida Man Arrested For Botched Castration On Another Man He Met On Eunuch Fetish Website',
+				link: 'https://www.cbsnews.com/miami/news/florida-man-arrested-botched-castration-eunuch-fetish-website/',
+				year: 2019
+			}
+		]
+	},
+	{
+		day: 21,
+		month: 8,
+		data: [
+			{
+				title: 'Florida man arrested after botching in-home castration surgery',
+				link: 'https://kfor.com/news/florida-man-arrested-after-botching-in-home-castration-surgery-deputies-say/',
+				year: 2019
+			}
+		]
+	},
+	{
+		day: 22,
+		month: 8,
+		data: [
+			{
+				title: 'Shirtless Florida man rides motorcycle down highway while lying on his back',
+				link: 'https://www.miamiherald.com/news/state/florida/article217133725.html',
+				year: 2018
+			}
+		]
+	},
+	{
+		day: 23,
+		month: 8,
+		data: [
+			{
+				title:
+					"Florida man arrested for DUI wearing 'I'm the Reason the Beer's Always Gone' t-shirt",
+				link: 'https://cbs12.com/news/local/james-rix-jr-florida-man-arrested-for-dui-wearing-im-the-reason-the-beers-always-gone-t-shirt-marion-oaks-manor-ocala-marion-county-sheriffs-office-august-23-2023',
+				year: 2023
+			}
+		]
+	},
+	{
+		day: 24,
+		month: 8,
+		data: [
+			{
+				title:
+					'Florida man punches elderly victim for ‘holding up the line’ after social distancing confrontation at Publix',
+				link: 'https://www.clickorlando.com/news/local/2020/08/24/florida-man-punches-elderly-victim-for-holding-up-the-line-after-social-distancing-confrontation-at-publix-police-say/',
+				year: 2020
+			}
+		]
+	},
+	{
+		day: 25,
+		month: 8,
+		data: [
+			{
+				title: 'Florida man admits to killing sister after fight over electric bill',
+				link: 'https://www.local10.com/news/local/2023/08/25/police-florida-man-admits-to-killing-sister-after-fight-over-electric-bill/',
+				year: 2023
+			}
+		]
+	},
+	{
+		day: 26,
+		month: 8,
+		data: [
+			{
+				title: 'Pantsless Florida man, 71, caught spying on neighbor through her bathroom window',
+				link: 'https://nypost.com/2023/08/26/florida-man-david-lee-henning-caught-spying-on-neighbor-through-her-bathroom-window/',
+				year: 2023
+			}
+		]
+	},
+	{
+		day: 27,
+		month: 8,
+		data: [
+			{
+				title: 'Florida man arrested for trying to buy 8-year-old girl for $100,000',
+				link: 'https://nypost.com/2022/08/27/florida-man-hellmuth-kolb-offers-mom-100000-for-8-year-old-daughter/',
+				year: 2022
+			}
+		]
+	},
+	{
+		day: 28,
+		month: 8,
+		data: [
+			{
+				title: 'Florida man arrested with pound of cocaine, some kept on stack of bacon',
+				link: 'https://www.wftv.com/news/trending/florida-man-arrested-with-pound-cocaine-some-kept-stack-bacon-deputies-say/DTHZQL5X5RERZJ2JO6ZCG2D62Y/',
+				year: 2020
+			}
+		]
+	},
+	{
+		day: 29,
+		month: 8,
+		data: [
+			{
+				title: 'Florida man tries to conceal $1,000 cash in rectum',
+				link: 'https://www.clickorlando.com/news/2017/08/29/florida-man-tries-to-conceal-1000-cash-in-rectum-deputies-say/',
+				year: 2017
+			}
+		]
+	},
+	{
+		day: 30,
+		month: 8,
+		data: [
+			{
+				title: "Florida man arrested for giving girlfriend a 'wet willy'",
+				link: 'https://www.local10.com/news/2018/08/30/florida-man-arrested-for-giving-girlfriend-a-wet-willy/',
+				year: 2018
+			}
+		]
+	},
+	{
+		day: 31,
+		month: 8,
+		data: [
+			{
+				title: 'Florida Man Arrested for Constructive Possession of an SBR',
+				link: 'https://www.guntrustlawyer.com/florida-man-arrested-for-const/',
+				year: 2009
+			}
+		]
 	}
 ];
